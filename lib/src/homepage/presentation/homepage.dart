9eff98ecee6394f084f49d1b36541655ea853e48
Raihan Firdaus <r1aihan2003f1irdaus@gmail.com>
2024-08-27 18:33:56 +0700

    chore: Update homepage to handle authentication state changes and navigate to appropriate screens

--- lib/src/homepage/presentation/homepage.dart
+++ lib/src/homepage/presentation/homepage.dart
@@ -1,10 +1,27 @@
+import 'package:chat_app/core/injection/injection.dart';
+import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter/material.dart';
+import 'package:go_router/go_router.dart';
 
 class Homepage extends StatelessWidget {
   const Homepage({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return const Placeholder();
+    return Scaffold(
+      body: StreamBuilder(
+        stream: getIt<FirebaseAuth>().authStateChanges(),
+        builder: (context, snapshot) {
+          if (snapshot.hasData) {
+            context.go('/chat-screen');
+          } else {
+            context.go('/sign-in');
+          }
+          return const Center(
+            child: CircularProgressIndicator(),
+          );
+        },
+      ),
+    );
   }
 }
