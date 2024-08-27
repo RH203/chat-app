40c9bab997b32335b17b43148f84806cdbfef3d4
Raihan Firdaus <r1aihan2003f1irdaus@gmail.com>
2024-08-27 18:33:37 +0700

    chore: Register FirebaseAuth in dependency injection

--- lib/core/injection/injection.dart
+++ lib/core/injection/injection.dart
@@ -1,5 +1,6 @@
 import 'package:chat_app/core/router/router.dart';
 import 'package:chat_app/core/utils/helper_validator.dart';
+import 'package:firebase_auth/firebase_auth.dart';
 
 import 'package:get_it/get_it.dart';
 
@@ -8,4 +9,5 @@ final getIt = GetIt.instance;
 void setup() {
   getIt.registerSingleton<AppRouter>(AppRouter());
   getIt.registerSingleton<HelperValidator>(HelperValidator());
+  getIt.registerSingleton<FirebaseAuth>(FirebaseAuth.instance);
 }
