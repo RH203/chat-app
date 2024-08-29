9bd5d213a5c4f2aa1c65b5a4d34eca48855fb366
Raihan Firdaus <r1aihan2003f1irdaus@gmail.com>
2024-08-29 19:01:28 +0700

    refactor: Update injection setup to be asynchronous

--- lib/core/injection/injection.dart
+++ lib/core/injection/injection.dart
@@ -6,7 +6,7 @@ import 'package:get_it/get_it.dart';
 
 final getIt = GetIt.instance;
 
-void setup() {
+Future<void> setup() async {
   getIt.registerSingleton<AppRouter>(AppRouter());
   getIt.registerSingleton<HelperValidator>(HelperValidator());
   getIt.registerSingleton<FirebaseAuth>(FirebaseAuth.instance);
