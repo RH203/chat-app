260da509e83e2b81e33ca35c0d4442229f501465
Raihan Firdaus <r1aihan2003f1irdaus@gmail.com>
2024-09-03 12:00:06 +0700

    refactor: Initialize and dispose text controllers in SignUpPage

--- lib/src/homepage/presentation/homepage.dart
+++ lib/src/homepage/presentation/homepage.dart
@@ -1,3 +1,4 @@
+import 'package:chat_app/app_logger.dart';
 import 'package:chat_app/core/injection/injection.dart';
 import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter/material.dart';
@@ -9,19 +10,45 @@ class Homepage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: StreamBuilder(
+      body: StreamBuilder<User?>(
         stream: getIt<FirebaseAuth>().authStateChanges(),
         builder: (context, snapshot) {
-          WidgetsBinding.instance.addPostFrameCallback((_) {
-            if (snapshot.hasData) {
-              context.go('/chat-screen');
-            } else if (snapshot.hasError) {
+          // Debugging connection state
+          AppLogger.debug("Connection State: ${snapshot.connectionState}");
+
+          if (snapshot.hasError) {
+            AppLogger.error("Error: ${snapshot.error}");
+            WidgetsBinding.instance.addPostFrameCallback((_) {
               context.go('/sign-in');
-            }
+            });
+            return Center(child: Text("Error: ${snapshot.error}"));
+          }
+
+          if (snapshot.connectionState == ConnectionState.waiting) {
+            AppLogger.debug("Loading");
+            return const Center(
+              child: CircularProgressIndicator(),
+            );
+          }
+
+          if (snapshot.hasData && snapshot.data != null) {
+            AppLogger.debug("User is authenticated: ${snapshot.data?.email}");
+            WidgetsBinding.instance.addPostFrameCallback((_) {
+              context.go('/chat-screen');
+            });
+            return const Center(
+              child: CircularProgressIndicator(),
+            );
+          }
+
+          // If no user is authenticated, redirect to sign-in
+          AppLogger.debug("No user is authenticated, redirecting to sign-in");
+          WidgetsBinding.instance.addPostFrameCallback((_) {
+            context.go('/sign-in');
           });
-          return const Center(
-            child: CircularProgressIndicator(),
-          );
+
+          // Return a fallback UI
+          return const Center(child: Text("No user authenticated"));
         },
       ),
     );
