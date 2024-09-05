5fe9b342aed4d83faea5e14e82c563bb42951735
Raihan Firdaus <r1aihan2003f1irdaus@gmail.com>
2024-09-05 11:57:17 +0700

    refactor: Initialize and dispose text controllers in SignUpPage

--- lib/src/chatscreen/presentation/chat_screen.dart
+++ lib/src/chatscreen/presentation/chat_screen.dart
@@ -1,14 +1,32 @@
+import 'package:chat_app/app_logger.dart';
 import 'package:flutter/material.dart';
 import 'package:go_router/go_router.dart';
 
 class ChatScreen extends StatefulWidget {
-  const ChatScreen({super.key});
+  ChatScreen({super.key});
 
   @override
   State<ChatScreen> createState() => _ChatScreenState();
 }
 
 class _ChatScreenState extends State<ChatScreen> {
+  final _messageController = TextEditingController();
+
+  @override
+  void dispose() {
+    // TODO: implement disposez
+    _messageController.dispose();
+    super.dispose();
+  }
+
+  void _sendMessage() {
+    final _message = _messageController.text;
+    if (_message.trim().isEmpty) return;
+    AppLogger.debug(_message);
+
+    _messageController.clear();
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -20,8 +38,34 @@ class _ChatScreenState extends State<ChatScreen> {
           )
         ],
       ),
-      body: const Center(
-        child: Text("Chat screen"),
+      body: Column(
+        children: [
+          const Expanded(
+            child: Center(
+              child: Text("No message found"),
+            ),
+          ),
+          Row(
+            children: [
+              Expanded(
+                child: TextField(
+                  controller: _messageController,
+                  autocorrect: true,
+                  textCapitalization: TextCapitalization.sentences,
+                  enableSuggestions: true,
+                  decoration: const InputDecoration(
+                    contentPadding: EdgeInsets.only(left: 10),
+                    label: Text("Send a message"),
+                  ),
+                ),
+              ),
+              IconButton(
+                onPressed: _sendMessage,
+                icon: const Icon(Icons.send),
+              )
+            ],
+          )
+        ],
       ),
     );
   }
