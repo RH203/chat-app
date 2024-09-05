b708d072a9053ca8e0e47ab0f3b0c895f7b135ae
Raihan Firdaus <r1aihan2003f1irdaus@gmail.com>
2024-09-05 11:56:58 +0700

    chore: Update dependencies for cloud_firestore and loading_animation_widget

--- lib/core/injection/injection.dart
+++ lib/core/injection/injection.dart
@@ -1,6 +1,8 @@
 import 'package:chat_app/core/router/router.dart';
 import 'package:chat_app/core/utils/helper_validator.dart';
+import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:firebase_auth/firebase_auth.dart';
+import 'package:firebase_storage/firebase_storage.dart';
 
 import 'package:get_it/get_it.dart';
 
@@ -10,4 +12,6 @@ Future<void> setup() async {
   getIt.registerSingleton<AppRouter>(AppRouter());
   getIt.registerSingleton<HelperValidator>(HelperValidator());
   getIt.registerSingleton<FirebaseAuth>(FirebaseAuth.instance);
+  getIt.registerFactory<FirebaseStorage>(() => FirebaseStorage.instance);
+  getIt.registerSingleton<FirebaseFirestore>(FirebaseFirestore.instance);
 }
